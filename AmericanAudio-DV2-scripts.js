98f9b2df2adcdba7ebd0b6c3cf3caff6c2bcc589
zero <zeromemory@gmail.com>
2014-09-26 01:18:56 -0700

    Some initial success with volume levels

--- AmericanAudio-DV2-scripts.js
+++ AmericanAudio-DV2-scripts.js
@@ -2,5 +2,16 @@ function AmericanAudioDV2() {}
 
 AmericanAudioDV2.init = function (id) {
     midi.sendShortMsg(0x91, 0x0F, 0x5);
+
+    engine.connectControl("[Master]", "VuMeterL", "AmericanAudioDV2.leftVolume");
+    engine.connectControl("[Master]", "VuMeterR", "AmericanAudioDV2.rightVolume");
+}
+
+AmericanAudioDV2.leftVolume = function (value) {
+    midi.sendShortMsg(0x90, 0x0F, value * 10);
+}
+
+AmericanAudioDV2.rightVolume = function (value) {
+    midi.sendShortMsg(0x91, 0x10, value * 10);
 }
 
